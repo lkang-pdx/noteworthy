ed6e2b9af5e83df41a265270686f5369dc9468f4
Luna Kang <luna.kang.ca@gmail.com>
2017-03-18 13:10:22 -0700

    Elasticsearch

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
+require 'elasticsearch/model'
+
 class Note < ApplicationRecord
   belongs_to :user
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+Note.import force:true
