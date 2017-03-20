35d9b950671e22daad26bdd307d36f992d90545a
Luna Kang <luna.kang.ca@gmail.com>
2017-03-19 17:12:37 -0700

    Elasticsearch (#1)
    * Elasticsearch
    
    * Search Highlighting

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,42 @@
+require 'elasticsearch/model'
+
 class Note < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :user
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'content']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            content: {}
+          }
+        }
+      }
+    )
+  end
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :content, analyzer: 'english'
+    end
+  end
 end
+Note.__elasticsearch__.client.indices.delete index: Note.index_name rescue nil
+
+Note.__elasticsearch__.client.indices.create \
+  index: Note.index_name,
+  body: { settings: Note.settings.to_hash, mappings: Note.mappings.to_hash }
+Note.import force:true
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   resources :notes
+  get 'search', to: 'search#search'
 
   authenticated :user do
     root 'notes#index', as: 'authenticated_root'
   end
+  
   root 'welcome#index'
 end
